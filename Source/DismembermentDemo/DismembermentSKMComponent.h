0446ceafaa75f449b6b90aa6e0decc197672f175
RapidAdam1 <79019053+RapidAdam1@users.noreply.github.com>
2024-05-15 23:04:17 +0100

    AddedCPPFiles & Folder Restructure

---
+++ Source/DismembermentDemo/DismembermentSKMComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "DismembermentSKMComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DISMEMBERMENTDEMO_API UDismembermentSKMComponent : public USkeletalMeshComponent
+{
+	GENERATED_BODY()
+	
+};
