7488414da9fd3edb634d47600a3ab076bc296f9c
RapidAdam1 <79019053+RapidAdam1@users.noreply.github.com>
2024-05-20 17:47:59 +0100

    VFC Particles
    - Removed Null Assets

--- Source/DismembermentDemo/DismembermentDemo.Build.cs
+++ Source/DismembermentDemo/DismembermentDemo.Build.cs
@@ -8,7 +8,7 @@ public class DismembermentDemo : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","Niagara" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","Niagara","CableComponent" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
